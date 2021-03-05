06e9154ab5d729503b833a3a09ba8bbb09bd2c35
David Galvis <ivan.galvis@project44.com>
2021-03-04 21:58:16 -0500

    section 14. Updating state immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -36,6 +36,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter} />
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => {
+                        return <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                    })}
+                </ul>
             </div>
         );
     }
@@ -45,7 +52,8 @@ class Counter extends Component {
 //this is the part of the state we want to get from the global redux state
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
@@ -55,7 +63,10 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: dispatch({type: 'DECREMENT' }),
         //we can also pass a payload through
         onAddCounter: dispatch({type: 'ADD', value: 5 }),
-        onSubtractCounter: dispatch({type: 'SUBTRACT', value: 5 })
+        onSubtractCounter: dispatch({type: 'SUBTRACT', value: 5 }),
+        onStoreResult: dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: dispatch({type: 'DELETE_RESULT'})
+
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,25 +1,38 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
 
     switch (action.type){
         case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
+            const newSatate = Object.assign({},state);
+            newSatate.counter = state.counter + 1;
+            return newSatate;
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.value
             }
         case 'SUBTRACT':
             return {
+                ...state,
                 counter: state.counter - action.value
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat( {id: new Date(), value:state.counter} )
+            }
+        case 'DELETE_RESULT':
+            return {
+
             }
     }
     return state;
