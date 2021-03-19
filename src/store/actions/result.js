0f0316b6b2cebe6843be67e67043c662b7e88559
David Galvis <ivan.galvis@project44.com>
2021-03-18 23:37:12 -0500

    section 16. Where to Put Data Transforming Logic?

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -2,8 +2,13 @@ import {DELETE_RESULT, STORE_RESULT} from "./actionTypes";
 
 
 export const saveresult = (result) => {
+    //we can modify the date here in the action creators or in the reducer, it depends
+    //also the state should not be prepared too much here in this part of the code
+    // const updatedResult = result * 2;
+    //If we are dealing with async actions, then here is the best place to do it, otherwise
     return {
         type: STORE_RESULT,
+        // result: updatedResult
         result: result
     };
 }
--- src/store/reducers/reducerResults.js
+++ src/store/reducers/reducerResults.js
@@ -8,13 +8,15 @@ const reducerCounter = (state = initialState, action) => {
 
     switch (action.type) {
         case actionTypes.STORE_RESULT:
+            //manipulate and change data if needed
             return {
                 ...state,
                 //by using concat we ensure to change the state immutably instead of changing the original state
                 //here we are changing the way this stuff is being mapped because since the state is in other reducer now, and one reducer cannot access the
                 //state that is stored in other reducer, but one component can access all the states of all the reducers by the mappers of the state into props
                 //so, taking advantage of this, we can implement a way of passing that actual state into the reducer as a variable
-                results: state.results.concat({id: new Date(), value: action.result})
+                //we should update the data here if we are going to run sync code, and taking into account that the reducer is the one in charge of updating the state
+                results: state.results.concat({id: new Date(), value: action.result * 2})
             }
         case actionTypes.DELETE_RESULT:
             //This is a way to remove an element immutably from an array
