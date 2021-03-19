948826ac216c0fa04b651152dc3958b6de6489a5
David Galvis <ivan.galvis@project44.com>
2021-03-18 23:58:18 -0500

    section 16. Using Utility Functions

--- src/store/reducers/reducerResults.js
+++ src/store/reducers/reducerResults.js
@@ -1,4 +1,5 @@
-import * as actionTypes from '../actions/actionTypes'
+import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from "../utility";
 
 const initialState = {
     results: []
@@ -8,16 +9,17 @@ const reducerCounter = (state = initialState, action) => {
 
     switch (action.type) {
         case actionTypes.STORE_RESULT:
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})})
             //manipulate and change data if needed
-            return {
-                ...state,
+            // return {
+            //     ...state,
                 //by using concat we ensure to change the state immutably instead of changing the original state
                 //here we are changing the way this stuff is being mapped because since the state is in other reducer now, and one reducer cannot access the
                 //state that is stored in other reducer, but one component can access all the states of all the reducers by the mappers of the state into props
                 //so, taking advantage of this, we can implement a way of passing that actual state into the reducer as a variable
                 //we should update the data here if we are going to run sync code, and taking into account that the reducer is the one in charge of updating the state
-                results: state.results.concat({id: new Date(), value: action.result * 2})
-            }
+            //     results: state.results.concat({id: new Date(), value: action.result * 2})
+            // }
         case actionTypes.DELETE_RESULT:
             //This is a way to remove an element immutably from an array
             // const id = 2;
@@ -25,10 +27,7 @@ const reducerCounter = (state = initialState, action) => {
             // const updatedArray = newArray.splice(id,1)
             //This is the most common way to remove an element from an array in an immutable way
             const updatedArray = state.results.filter(result => result.id !== action.resultElId)
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject(state, {results: updatedArray});
     }
     return state;
 }
